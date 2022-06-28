460a06aa714a37b1238bdfb20f33ed914a17bc12
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-06-28 10:33:14 +0500

    new

--- config/event/models.py
+++ config/event/models.py
@@ -1,2 +1,8 @@
 from django.db import models
-# Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
